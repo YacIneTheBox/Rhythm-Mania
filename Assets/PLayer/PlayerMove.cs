481de818bff9678a0493c7f85d2eef116855b3e4
YacIneTheBox <yacaitali@gmail.com>
2025-10-28 13:38:51 +0100

    Initial Commit

---
+++ Assets/PLayer/PlayerMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    Rigidbody2D rb;
+    float speedx, speedy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedx = Input.GetAxis("Horizontal") * moveSpeed;
+        speedy = Input.GetAxis("Vertical") * moveSpeed;
+        rb.linearVelocity = new Vector2(speedx, speedy);
+    }
+}
